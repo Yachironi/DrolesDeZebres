c51ae6d4894f734d9783e18274944dd5c70fdfea
Yasser RABI <yachironi@hotmail.com>
2015-01-31 23:18:36 +0100

    Initialisation

---
+++ DrolesDeZebres/Headers/JeuxFabrique.h
@@ -0,0 +1,20 @@
+/***********************************************************************
+ * Module:  JeuxFabrique.h
+ * Author:  Yasser RABI & Zheng ZHANG
+ * Modified: samedi 31 janvier 2015 10:55:01
+ * Purpose: Declaration of the class JeuxFabrique
+ ***********************************************************************/
+
+#if !defined(__ImpalaJones_JeuxFabrique_h)
+#define __ImpalaJones_JeuxFabrique_h
+using namespace std;
+
+class JeuxFabrique
+{
+public:
+protected:
+private:
+
+};
+
+#endif
---
+++ DrolesDeZebres/Headers/Lion.h
@@ -0,0 +1,23 @@
+/***********************************************************************
+ * Module:  Lion.h
+ * Author:  Yasser RABI & Zheng ZHANG
+ * Modified: vendredi 30 janvier 2015 12:04:25
+ * Purpose: Declaration of the class Lion
+ ***********************************************************************/
+
+#if !defined(__ImpalaJones_Lion_h)
+#define __ImpalaJones_Lion_h
+
+using namespace std;
+
+#include "Animal.h"
+
+class Lion : public Animal
+{
+public:
+protected:
+private:
+
+};
+
+#endif
