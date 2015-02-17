bd58ad6c4ab1244d6f64c764b839c3ca52d753aa
zzcoolj <zzcooljay@gmail.com>
2015-02-17 19:39:16 +0100

    plateau

--- Headers/Plateau.h
+++ Headers/Plateau.h
@@ -62,7 +62,8 @@ public:
 	}
 
 	friend ostream& operator<<(ostream& os, const Plateau& plateau) {
-			char c[11][13];
+		/*
+		char c[11][13];
 					for (int i = 0; i < 5; i++){
 						for (int j = 0; j < 5; j++){
 							if (plateau.cases[i][j]->getIdSecteur() != plateau.cases[i][j + 1]->getIdSecteur()){
@@ -130,79 +131,111 @@ public:
 					}
 					os << endl;
 					return os;
-		}
+					*/
 
-	/*
-	void afficher() {
-		//char c[9][11];
-		char c[11][13];
-		for (int i = 0; i < 5; i++){
-			for (int j = 0; j < 5; j++){
-				if (cases[i][j]->getIdSecteur() != cases[i][j + 1]->getIdSecteur()){
-					c[2 * i + 1][2 * j + 2] = '|';
-				}
-				else c[2 * i + 1][2 * j + 2] = ' ';
-			}
-		}
+				string c[13][35];
+							for (int i = 0; i < 5; i++){
+								for (int j = 0; j < 5; j++){
+									if (plateau.cases[i][j]->getIdSecteur() != plateau.cases[i][j + 1]->getIdSecteur()){
+										c[2 * i + 2][5 * j + 1 + 6] = '|';
+									}
+									else c[2 * i + 2][5 * j + 1 + 6] = ' ';
+								}
+							}
 
-		for (int i = 0; i < 6; i++){
-			for (int j = 0; j < 4; j++){
-				c[2 * j + 2][2 * i] = ' ';
-				if (cases[j][i]->getIdSecteur() != cases[j + 1][i]->getIdSecteur()){
-					c[2 * j + 2][2 * i + 1] = '-';
-				}
-				else c[2 * j + 2][2 * i + 1] = ' ';
-			}
-		}
+							for (int i = 0; i < 6; i++){
+								for (int j = 0; j < 4; j++){
+									c[2 * j + 3][5 * i + 2] = ' ';
+									if (plateau.cases[j][i]->getIdSecteur() != plateau.cases[j + 1][i]->getIdSecteur()){
+										c[2 * j + 3][5 * i + 3] = "----";
+									}
+									else c[2 * j + 3][5 * i + 3] = "    ";
+								}
+							}
 
-		for (int i = 0; i < 5; i++){
-			for (int j = 0; j < 6; j++){
-				if (cases[i][j]->pion == NULL){
-					c[2 * i + 1][2 * j + 1] = '0';
-				}
-				else if(cases[i][j]->pion->isCache()){
-					c[2 * i + 1][2 * j + 1] = '#';
-				}
-				else {
-					string pionType = cases[i][j]->pion->getIntitulePion();
-					//Le Zebre
-					if (pionType == "Zebre") c[2 * i + 1][2 * j + 1] = 'Z';
-					//La Gazelle
-					else if (pionType == "Gazelle") c[2 * i + 1][2 * j + 1] = 'G';
-					//Le Lion
-					else if (pionType == "Lion") c[2 * i + 1][2 * j + 1] = 'L';
-					//L'Elephant
-					else if (pionType == "Elephant") c[2 * i + 1][2 * j + 1] = 'L';
-					//Le Crocodile	
-					else if (pionType == "Elephant") c[2 * i + 1][2 * j + 1] = '0';
-				}
-			}
-		}
+							for (int i = 0; i < 5; i++){
+								for (int j = 0; j < 6; j++){
+									if (plateau.cases[i][j]->pion == NULL){
+										c[2 * i + 2][5 * j + 3] = "0(0)";
 
-		// afficher le chemin d'Impala Jones
-		c[0][0] = 'X';
-		c[0][12] = 'X';
-		c[10][0] = 'X';
-		c[10][12] = 'X';
+									}
+									else if(plateau.cases[i][j]->pion->isCache()){
+										c[2 * i + 2][5 * j + 3] = "#(0)";
+									}
+									//TODO change!!!
+									else {
+										string pionType = plateau.cases[i][j]->pion->getIntitulePion();
+										//Le Zebre
+										if (pionType == "Zebre") c[2 * i + 2][2 * j + 3] = 'Z';
+										//La Gazelle
+										else if (pionType == "Gazelle") c[2 * i + 2][2 * j + 3] = 'G';
+										//Le Lion
+										else if (pionType == "Lion") c[2 * i + 2][2 * j + 3] = 'L';
+										//L'Elephant
+										else if (pionType == "Elephant") c[2 * i + 2][2 * j + 3] = 'L';
+										//Le Crocodile
+										else if (pionType == "Elephant") c[2 * i + 2][2 * j + 3] = '0';
+									}
+								}
+							}
 
-		for (int i = 1; i < 12; i++){
-			c[0][i] = ' ';
-			c[10][i] = ' ';
-		}
-		for (int i = 1; i < 10; i++){
-			c[i][0] = ' ';
-			c[i][12] = ' ';
-		}
+							//afficher la frontiere
+							for (int i = 1; i < 11; i++){
+								c[i][2] = '|';
+								c[i][32] = '|';
+							}
+							for(int i =1;i<31;i++){
+								c[1][i+1] = "-";
+								c[11][i+1] = "-";
+							}
+							c[1][2] = 'X';
+							c[1][32] = 'X';
+							c[11][2] = 'X';
+							c[11][32] = 'X';
 
-		// afficher la table de char 
-		for (int i = 0; i < 11; i++){
-			cout << endl;
-			for (int j = 0; j < 13; j++){
-				cout << c[i][j];
-			}
+
+							//afficher le chemin d'Impala Jones
+							c[0][0] = 'X';
+							c[0][34] = 'X';
+							c[12][0] = 'X';
+							c[12][34] = 'X';
+							for (int i = 1; i < 34; i++){
+									c[0][i] = ' ';
+									c[12][i] = ' ';
+							}
+							for (int i = 1; i < 12; i++){
+									c[i][0] = "  ";
+									c[i][33] = "  ";
+							}
+
+							c[0][4] = '0';
+							c[0][9] = '1';
+							c[0][14] = '2';
+							c[0][19] = '3';
+							c[0][24] = '4';
+							c[0][29] = '5';
+							c[2][33] = '6';
+							c[4][33] = '7';
+							c[6][33] = '8';
+							c[8][33] = '9';
+							c[10][33] = "10";
+							c[1][29] = '5';
+							c[10][0] = "17";
+							c[8][0] = "18";
+							c[6][0] = "19";
+							c[4][0] = "20";
+							c[2][0] = "21";
+
+							// afficher la table de char
+							for (int i = 0; i < 13; i++){
+								os << endl;
+								for (int j = 0; j < 35; j++){
+									os << c[i][j];
+								}
+							}
+							os << endl;
+							return os;
 		}
-		cout << endl;
-	}*/
 
 protected:
 private:
